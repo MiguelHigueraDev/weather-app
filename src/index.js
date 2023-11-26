1539b27f8e3d07f560d93a545b1ad7b08202934b
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2023-11-26 08:34:27 -0300

    Add search city

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+import CONSTANTS from './constants';
 import chartManager from './modules/chartManager';
 import displayManager from './modules/displayManager';
+import forecastManager from './modules/forecastManager';
+
+const searchModalBtn = document.querySelector('#search');
+const searchModal = document.querySelector('#city-search');
+const searchInput = document.querySelector('#city');
+const searchBtn = document.querySelector('#get-weather-btn');
+
+searchModalBtn.addEventListener('click', () => {
+  searchInput.value = '';
+  searchModal.showModal();
+});
+
+searchBtn.addEventListener('click', () => {
+  if (searchInput.value.length > 2) {
+    forecastManager.setSavedLocation(searchInput.value);
+    displayManager.updateForecast(CONSTANTS.API_KEY);
+    searchModal.close();
+  }
+});
 
 displayManager.startUI();
-
 chartManager.createChart();
