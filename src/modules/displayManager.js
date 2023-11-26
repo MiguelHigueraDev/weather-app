4f43249c48ce0859f9ef22ffdd12d3b8af0ee515
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2023-11-26 19:41:37 -0300

    Add transition

--- src/modules/displayManager.js
+++ src/modules/displayManager.js
@@ -11,6 +11,8 @@ const temperatureBtn = document.querySelector('#temperature-btn');
 const precipitationBtn = document.querySelector('#precipitation-btn');
 const windBtn = document.querySelector('#wind-btn');
 
+const animatedDivs = document.querySelectorAll('.animated');
+
 const getWeatherIcon = (code) => {
   const { icon } = CONSTANTS.WEATHER_CODES.find((w) => w.code === code);
   return icon;
@@ -28,6 +30,18 @@ function resetSelectedBtn() {
   temperatureBtn.classList.remove('selected-info');
   precipitationBtn.classList.remove('selected-info');
   windBtn.classList.remove('selected-info');
+}
+
+function toggleVisibility(bool) {
+  animatedDivs.forEach((div) => {
+    if (bool) {
+      div.classList.add('visible');
+      div.classList.remove('transparent');
+    } else {
+      div.classList.add('transparent');
+      div.classList.remove('visible');
+    }
+  });
 }
 
 async function updateChart(type) {
@@ -69,6 +83,7 @@ function updateDayForecast(index, icon, text, day, max, min) {
 
 async function updateForecast(apiKey) {
   const local = localStorage.getItem('location');
+  toggleVisibility(false);
   const dailyForecast = await forecastManager.getDailyForecast(apiKey, local);
   updateHeader(...forecastManager.getHeaderInformation(dailyForecast));
   updateChart('temperature');
@@ -76,6 +91,7 @@ async function updateForecast(apiKey) {
   updateDayForecast(1, ...forecastManager.getDayForecast(dailyForecast, 1));
   updateDayForecast(2, ...forecastManager.getDayForecast(dailyForecast, 2));
   updateDayForecast(3, ...forecastManager.getDayForecast(dailyForecast, 3));
+  toggleVisibility(true);
 }
 
 function startUI() {
